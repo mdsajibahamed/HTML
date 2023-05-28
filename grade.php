31dade34725f706f8ac27e05a75d67a2ca350e48
mdsajib <mdsojib918739@gmail.com>
2023-05-28 11:46:25 +0600

    second day

--- grade.php
+++ grade.php
@@ -38,5 +38,18 @@
 
 
     ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus corrupti fuga non. Voluptates temporibus unde consequuntur laborum? Consequuntur nostrum dolorum aut excepturi reprehenderit reiciendis consequatur. Culpa quam alias incidunt est.</p>
+    </body>
+    </html>
 </body>
 </html>
