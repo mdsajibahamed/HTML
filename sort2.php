ba3ffeda3ef3390d89404a84d47f84a0ce0f9732
MD . SAJIB <125546611+mdsajibahamed@users.noreply.github.com>
2023-10-04 23:56:18 +0600

    Update sort2.php
    country added

--- sort2.php
+++ sort2.php
@@ -1,8 +1,7 @@
 <?php
-$name = array(" bangladesh" => "dhaka","pakistan"=>"Islamabad","afganistan"=>"Kabul","iran"=>"Theran");
+$name = array(" bangladesh" => "dhaka","pakistan"=>"Islamabad","afganistan"=>"Kabul","iran"=>"Theran","Nepal"=> "Khatmondu","Sri-Lanka"=>"Colombo","Vhutan"=>"Thimpu");
 ksort($name);
 foreach ($name as $k=>$v){
     echo "key is :".$k." value is :".$v."<br>";
 }
-
 ?>
