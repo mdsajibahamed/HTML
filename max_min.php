53ae64017ed804e27e348d485f93131882df6711
MD . SAJIB <125546611+mdsajibahamed@users.noreply.github.com>
2023-10-05 00:02:53 +0600

    Update max_min.php

--- max_min.php
+++ max_min.php
@@ -5,19 +5,19 @@ $b =60;
 $c =40;
 
 if($a>$b && $a>$c){
-    echo "maximum value is ".$a;
+    echo "maximum value is a = ".$a;
 }elseif($b>$a && $b>$c){
     echo "maximum value is b = ".$b;
 }else{
-    echo "maximun value is ".$c;
+    echo "maximun value is c = ".$c;
 }
 echo "<hr>";
 if($a<$b && $a<$c){
-    echo "minimum value is ".$a;
+    echo "minimum value is a = ".$a;
 }elseif($b<$a && $b<$c){
     echo "minimum value is b = ".$b;
 }else{
-    echo "minimun value is ".$c;
+    echo "minimun value is c = ".$c;
 }
 
 ?>
