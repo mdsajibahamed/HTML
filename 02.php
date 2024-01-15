20dd55462a9dc62fc41cb1ec87ba03489502d5ab
MD . SAJIB <125546611+mdsajibahamed@users.noreply.github.com>
2024-01-15 23:30:53 +0600

    Update 02.php

--- 02.php
+++ 02.php
@@ -7,7 +7,6 @@ if(isset($_POST['calc'])){
     if($fNumber >= $sNumber && $fNumber >= $tNumber){
         $maxVal = $fNumber;
     }
-        
     elseif($sNumber >= $fNumber && $sNumber >= $tNumber){
         $maxVal = $sNumber;
     }
